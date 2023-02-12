ad2e7b110bc4b2f7e718a79f70f183c9d9068e8e
Himanshu Shukla <91239771+hs0047@users.noreply.github.com>
2023-02-12 20:27:44 +0530

    HS047-init change

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+const https = require('http');
+
+const server = https.createServer((req, res) => {
+    req.statusCode(200);
+    req.setHeader('Content-Type', 'Text/Plain');
+    res.end("Namaste")
+})
+
+const PORT = process.env.SERVER_PORT || 3002;
+
+server.listen(PORT, () => {
+    console.log(`Server is Running on Port ${PORT}`)
+})
